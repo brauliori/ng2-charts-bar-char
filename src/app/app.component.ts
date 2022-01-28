7a8c3331e081ecbd42887e00df94a1196372eff7
brauliori <brauliori@gmail.com>
2022-01-28 16:06:29 -0300

    3 files modified, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { Label } from 'ng2-charts';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   public barChartOptions: ChartOptions = {
@@ -30,7 +29,9 @@ export class AppComponent implements OnInit {
         label: function (tooltipItem, data) {
           var dataset = data.datasets[tooltipItem.datasetIndex];
           var currentValue = dataset.data[tooltipItem.index];
-          return currentValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+          console.log(dataset.tasa)
+
+          return new Intl.NumberFormat('es-CL').format(tooltipItem.yLabel) + " Tasa: " + dataset.tasa[tooltipItem.index];
         },
       },
     },
@@ -51,11 +52,23 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.barChartData = [
-      { data: [20000, 10000, 5000, 3000], label: '100.000' },
-      { data: [60000, 30000, 15000, 5000], label: '200.000' },
-      { data: [80000, 40000, 20000, 10000], label: '300.000' },
-      { data: [100000, 50000, 25000, 12500], label: '400.000' },
+      { data: [], label: '' ,tasa:[]},
+      { data: [], label: '',tasa:[] },
+      { data: [], label: '',tasa:[] },
+      { data: [], label: '',tasa:[] },
     ];
     this.barChartLabels = ['12', '26', '36', '48'];
   }
+
+  
+
+  cambiar() {
+    this.barChartData = [
+      { data: [20000, 10000, 5000, 3000], label: '100.000' ,tasa:[1,2,3,4]},
+      { data: [60000, 30000, 15000, 5000], label: '200.000',tasa:[5,6,7,8] },
+      { data: [80000, 40000, 20000, 10000], label: '300.000',tasa:[9,10,11,12] },
+      { data: [100000, 50000, 25000, 12500], label: '400.000',tasa:[13,14,15,16]}
+    ];
+    this.barChartLabels = ['12', '26', '36', '48'];
+  } 
 }
